236d97c83acbf10852970decea4282d34ac7812b
Alexander Moller <amloelxer@gmail.com>
2016-10-26 16:30:50 -0600

    maybe have let and lambda and call done? Idk

--- Midterm.rkt
+++ Midterm.rkt
@@ -31,13 +31,15 @@
 (struct ifExpression(firstArgument secondArgument thirdArgument)#:transparent) 
 
  ;;; Let a(first Expression) =(second expression) b (third expression)
-(struct letExpression(firstArugment secondArgument ))
+(struct letExpression(firstArugment secondArgument thirdArgument))
 (struct lambdaExpression(firstArgument secondArgument))
 
 ;;Width is part of call
 (struct callExpression(firstArgument secondArgument))
 
 (struct valueExpression(firstArgument))
+
+(struct idExpression(firstArgument))
 
 
 ; Implementation of Environment
@@ -110,13 +112,17 @@
                                                      [else (xorExpression $2 $4)]))
 
      ;;;Lambda
-     ((LEFTPAREN LAMBDA LEFTPAREN ID RIGHTPAREN exp) (lambdaExpression $4 $6))
+     ((LEFTPAREN LAMBDA LEFTPAREN ID RIGHTPAREN exp RIGHTPAREN) (lambdaExpression $4 $6))
+
+
+
+     
      ;;;Let
      ((LEFTPAREN LET LEFTPAREN ID VALUE RIGHTPAREN IN exp RIGHTPAREN) (cond
-                                                                        [(equal? $5 "λ") (lambdaExpression $4 $8)]
-                                                                        [(equal? $5 "&") (andExpression $4 $8)]
-                                                                        [(equal? $5 "|") (orExpression $4 $8)]
-                                                                        [(equal? $5 "^") (xorExpression $4 $8)]
+                                                                        [(equal? $5 "λ") (lambdaExpression $4 $5 $8)]
+                                                                        [(equal? $5 "&") (andExpression $4 $5 $8)]
+                                                                        [(equal? $5 "|") (orExpression $4 $5 $8)]
+                                                                        [(equal? $5 "^") (xorExpression $4 $5 $8)]
                                                                         [(equal? $5 "true") (valueExpression $5)]
                                                                         [(equal? $5 "false") (xorExpression $5)]
                                                                         [(equal? $5 "!") (unaryExpression $8)]))
@@ -126,6 +132,9 @@
      ((LEFTPAREN IF exp THEN exp ELSE exp RIGHTPAREN) (ifExpression $3 $5 $7))
      ;;; Call
      ((LEFTPAREN ID WITH exp RIGHTPAREN) (callExpression $2 $4))
+
+     ;;IDExpression
+     ((ID) (idExpression $1))
      
      )
     )
@@ -133,28 +142,52 @@
   )
 
 
+(define apply-env
+  (lambda (env search-var)
+    (cond
+      ((eqv? (car env) 'empty-env)
+       (error "No Binding Found: ~s" search-var))
+      ((eqv? (car env) 'extend-env)
+       (let ((saved-var (cadr env))
+             (saved-val (caddr env))
+             (saved-env (cadddr env)))
+         (if (eqv? search-var saved-var)
+             saved-val
+             (apply-env saved-env search-var))))
+      (else
+       (error "Invalid Environment: ~s"  env)))))
 
 (define (evaluate aTree env)
   (match aTree
     ;;; Base Case
-      [(unaryExpression argumentOne) (not (evaluate argumentOne))]
+      [(unaryExpression argumentOne) (not (evaluate argumentOne empty-env))]
     ;;; Returns true or false
       [(valueExpression argumentOne) (equal? argumentOne "true")]
       ;;Lambda
-      [(lambdaExpression argumentOne argumentTwo) (lambda (val) (evaluate argumentTwo (extend-env argumentOne val env)))] ;;;something))] ;;;nlank))]
-    ;;; And
-     [(andExpression argumentOne argumentTwo) (and (evaluate argumentOne) (evaluate argumentTwo))]
+      [(lambdaExpression argumentOne argumentTwo) (lambda (val) (evaluate argumentTwo (extend-env argumentOne val env)))]
+      ;;;
+   
+   
+    [(idExpression argumentOne) (apply-env env argumentOne)]
+    
+    ;;;Let Expression
+    [(letExpression argumentOne argumentTwo argumentThree) (evaluate argumentThree (extend-env argumentTwo argumentThree))]
+
+     ;;; And
+     [(andExpression argumentOne argumentTwo) (and (evaluate argumentOne empty-env) (evaluate argumentTwo empty-env))]
     ;;; OR
-     [(orExpression argumentOne argumentTwo) (or (evaluate argumentOne) (evaluate argumentTwo))]
+     [(orExpression argumentOne argumentTwo) (or (evaluate argumentOne empty-env) (evaluate argumentTwo empty-env))]
     ;;; XOR
-    [(xorExpression argumentOne argumentTwo) (xor (evaluate argumentOne) (evaluate argumentTwo))]
+    [(xorExpression argumentOne argumentTwo) (xor (evaluate argumentOne empty-env) (evaluate argumentTwo empty-env))]
     ;;; IF
-    [(ifExpression firstArgument secondArgument thirdArgument) (if (evaluate firstArgument) (evaluate secondArgument) (evaluate thirdArgument))]
+    [(ifExpression firstArgument secondArgument thirdArgument) (if (evaluate firstArgument empty-env) (evaluate secondArgument empty-env) (evaluate thirdArgument empty-env))]
+
+    [(callExpression firstArgument secondArgument) (extend-env firstArgument evaluate(secondArgument env))]
 
   
 ))
 
-;;;(evaluate (expparser (lex-this constantBoolLexer testName)))
+;;;(evaluate (expparser (lex-this constantBoolLexer nottest)) (empty-env))
 (define nottest (open-input-string "(! false)"))
 (define andTest (open-input-string "(false & true)"))
 (define orTest (open-input-string "(false or true)"))
